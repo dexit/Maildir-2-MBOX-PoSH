74153ffdbf08a964625c91a520ebbbfc412986bc
dExIT <dexit@dyc.lv>
2021-02-04 10:24:53 +0000

    Initial commit

---
+++ MaildirToMbox.ps1
@@ -0,0 +1,7 @@
+﻿ $table = Get-ChildItem -Path "pathto\domain.com\user" -Recurse -Directory -Include .* -Force -ErrorAction SilentlyContinue | Select-Object FullName, Name
+foreach ($row in $table)
+{
+  
+python -m maildir2mbox $($row.FullName) "C:\testt\$($row.Name.substring(1)).mbox"
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Maildir 2 MBOX PoSH
- 
+# Maildir 2 MBOX PoSH 
+This is a simple PoSH wrapper for  Py3 ext maildir2mbox.
+
+# Needed:
++ PoSH
++ Py3+
++ Py ext maildir2mbox
